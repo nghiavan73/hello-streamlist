f6ba79364fa709eeb94b8ebaf4f0ffb243e71dd3
nghiavan73 <157226027+nghiavan73@users.noreply.github.com>
2024-05-01 19:01:03 +0000

    Try to hide Demo bar

--- utils.py
+++ utils.py
@@ -19,10 +19,11 @@ import streamlit as st
 
 
 def show_code(demo):
+    nv = "Do nothing"
     """Showing the code of the demo."""
-    show_code = st.sidebar.checkbox("Show code", True)
+    '''show_code = st.sidebar.checkbox("Show code", True)
     if show_code:
         # Showing the code of the demo.
         st.markdown("## Code")
         sourcelines, _ = inspect.getsourcelines(demo)
-        st.code(textwrap.dedent("".join(sourcelines[1:])))
+        st.code(textwrap.dedent("".join(sourcelines[1:])))'''
